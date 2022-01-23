697673615bf4f57d9464d8114c9fa27675830baa
Jesse Zeferino Rodriguez <jesse.xd.jz@gmail.com>
2022-01-23 14:54:26 -0600

    subir el proyecto

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,26 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { NgCalendarModule  } from 'ionic2-calendar';
+import { CalModalPageModule } from '../pages/cal-modal/cal-modal.module';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+registerLocaleData(localeDe);
+@NgModule({
+  imports: [
+    NgCalendarModule,
+    CalModalPageModule,
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage],providers:[
+    { provide: LOCALE_ID, useValue: 'de-DE' }
+  ]
+})
+export class HomePageModule {}
